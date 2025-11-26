c6a307ec4569ab845c062dd0c0d54ba7c3d07eca
Mqiyi <203073839@qq.com>
2025-11-26 22:21:03 +0800

    Update PoweredBy.js

--- components/PoweredBy.js
+++ components/PoweredBy.js
@@ -1,19 +1,9 @@
 import { siteConfig } from '@/lib/config'
 
-/**
- * 驱动版权
- * @returns
- */
 export default function PoweredBy(props) {
   return (
-    <div className={`inline text-sm font-serif ${props.className || ''}`}>
-      <span className='mr-1'>Powered by</span>
-      <a
-        href='https://github.com/tangly1024/NotionNext'
-        className='underline justify-start'>
-        NotionNext {siteConfig('VERSION')}
-      </a>
-      .
+    <div className={`inline text-sm font-serif opacity-70 ${props.className || ''}`}>
+      © {new Date().getFullYear()} 梦祈亦 · 版权所有
     </div>
   )
 }
