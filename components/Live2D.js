f2ff25e8020a2bb940528d060de0fb499ec2aad6
Mqiyi <203073839@qq.com>
2025-11-26 23:00:04 +0800

    Update Live2D.js

--- components/Live2D.js
+++ components/Live2D.js
@@ -4,45 +4,33 @@ import { useGlobal } from '@/lib/global'
 import { isMobile, loadExternalResource } from '@/lib/utils'
 import { useEffect } from 'react'
 
-/**
- * 网页动画
- * @returns
- */
 export default function Live2D() {
-  const { theme, switchTheme } = useGlobal()
+  const { theme } = useGlobal()
   const showPet = JSON.parse(siteConfig('WIDGET_PET'))
-  const petLink = siteConfig('WIDGET_PET_LINK')
-  const petSwitchTheme = siteConfig('WIDGET_PET_SWITCH_THEME')
 
   useEffect(() => {
     if (showPet && !isMobile()) {
-      Promise.all([
-        loadExternalResource(
-          'https://cdn.jsdelivr.net/gh/stevenjoezhang/live2d-widget@latest/live2d.min.js',
-          'js'
-        )
-      ]).then(e => {
-        if (typeof window?.loadlive2d !== 'undefined') {
-          // https://github.com/xiazeyu/live2d-widget-models
-          try {
-            loadlive2d('live2d', petLink)
-          } catch (error) {
-            console.error('读取PET模型', error)
-          }
+      loadExternalResource(
+        'https://cdn.jsdelivr.net/gh/stevenjoezhang/live2d-widget@latest/live2d.min.js',
+        'js'
+      ).then(() => {
+        try {
+          // 初始化 Live2D 模型
+          loadlive2d('live2d', siteConfig('WIDGET_PET_MODEL'))
+        } catch (error) {
+          console.error('读取 PET 模型失败', error)
         }
       })
     }
   }, [theme])
 
   function handleClick() {
-    if (petSwitchTheme) {
-      switchTheme()
-    }
+    // 从后台配置获取跳转链接，默认 https://q.2030777.xyz
+    const link = siteConfig('WIDGET_PET_CLICK_LINK') || 'https://q.2030777.xyz'
+    window.open(link, '_blank')
   }
 
-  if (!showPet) {
-    return <></>
-  }
+  if (!showPet) return <></>
 
   return (
     <canvas
